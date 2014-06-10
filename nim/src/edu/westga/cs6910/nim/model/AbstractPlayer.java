16f41a06e18ceb06057eddedaf955408f99cebed
april_000 <april_000@localhost>
2014-06-10 12:14:25 -0400

    Changed name

---
+++ nim/src/edu/westga/cs6910/nim/model/AbstractPlayer.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package edu.westga.cs6910.nim.model;
+
+/**
+ * Abstract superclass defining different aspects of a player.
+ * 
+ * @author April Parmer
+ * @version Summer 2014
+ *
+ */
+public abstract class AbstractPlayer {
+
+}
