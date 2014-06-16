d856bbb9b09f955e8339842add123c7a1f5dc90b
april_000 <april_000@localhost>
2014-06-16 13:25:00 -0400

    Passed second test - creation of Pile with 10 sticks

---
+++ nim/src/edu/westga/cs6910/nim/testing/pile/PilesCreated.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package edu.westga.cs6910.nim.testing.pile;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.westga.cs6910.nim.model.Pile;
+
+/**
+ * This class tests the creation of a Constructor in the Pile class.
+ * 
+ * @author April Parmer
+ * @version Summer 2014
+ *
+ */
+public class PilesCreated {
+
+	/**
+	 * Tests the creation of a Pile with 1 stick.
+	 */
+	@Test
+	public void testShouldProducePileWith1Stick() {
+		Pile oneStickPile = new Pile(1);
+		assertEquals("Pile size: 1", oneStickPile.toString());
+	}
+	
+	/**
+	 * Tests the creation of a Pile with 10 sticks.
+	 */
+	@Test
+	public void testShouldProducePileWith10Sticks() {
+		Pile tenStickPile = new Pile(10);
+		assertEquals("Pile size: 10", tenStickPile.toString());
+	}
+
+}
