4e77dcb4089dc0b1ab85f8080872bf7d3248c0b5
april_000 <april_000@localhost>
2014-06-16 14:17:08 -0400

    Initial commit
    Passed first test of creating a HumanPlayer named John

---
+++ nim/src/edu/westga/cs6910/nim/testing/HumanPlayer/HumanPlayerCreated.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package edu.westga.cs6910.nim.testing.HumanPlayer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.westga.cs6910.nim.model.AbstractPlayer;
+import edu.westga.cs6910.nim.model.HumanPlayer;
+
+/**
+ * Tests the creation of a HumanPlayer by calling the toString method.
+ * 
+ * @author April Parmer
+ * @version Summer 2014
+ *
+ */
+public class HumanPlayerCreated {
+
+	/**
+	 * Creates a HumanPlayer with the name of "John" and 0 sticks to take.
+	 */
+	@Test
+	public void testCreatHumanPlayerJohnWith0Sticks() {
+		AbstractPlayer john = new HumanPlayer("John");
+		assertEquals("Human Player named: John with 0 sticks to take", john.toString());
+	}
+
+}
