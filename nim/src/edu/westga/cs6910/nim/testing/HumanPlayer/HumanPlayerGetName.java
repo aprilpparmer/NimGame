e26ce841c08bfb3f0528a4ba789acca45b3a7a18
april_000 <april_000@localhost>
2014-06-16 14:25:31 -0400

    Passed first test - getName = Sally

---
+++ nim/src/edu/westga/cs6910/nim/testing/HumanPlayer/HumanPlayerGetName.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package edu.westga.cs6910.nim.testing.HumanPlayer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.westga.cs6910.nim.model.AbstractPlayer;
+import edu.westga.cs6910.nim.model.HumanPlayer;
+
+/**
+ * Tests the functionality of the getName method of the HumanPlayer class.
+ * 
+ * @author April Parmer
+ * @version Summer 2014
+ *
+ */
+public class HumanPlayerGetName {
+
+	/**
+	 * Returns the name of a human player named Sally.
+	 */
+	@Test
+	public void testHumanPlayerGetNameSally() {
+		AbstractPlayer sally = new HumanPlayer("Sally");
+		assertEquals("Sally", sally.getName());
+	}
+
+}
