49040a8056a638b295888b9bec789a4b6dfe7270
april_000 <april_000@localhost>
2014-06-16 13:20:20 -0400

    Initial commit

---
+++ nim/src/edu/westga/cs6910/nim/testing/pile/PileIsCreated.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.westga.cs6910.nim.testing.pile;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author april_000
+ *
+ */
+public class PileIsCreated {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
