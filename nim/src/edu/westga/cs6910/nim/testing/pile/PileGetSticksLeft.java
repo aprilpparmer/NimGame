f8e71610178bdd9b7601bce164e9cfafbc54abd6
april_000 <april_000@localhost>
2014-06-16 13:34:32 -0400

    Passed first two tests on getSticksLeft method of the pile class.

---
+++ nim/src/edu/westga/cs6910/nim/testing/pile/PileGetSticksLeft.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package edu.westga.cs6910.nim.testing.pile;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.westga.cs6910.nim.model.Pile;
+
+/**
+ * Tests the functionality of the getSticksLeft method in the Pile class.
+ * 
+ * @author April Parmer
+ * @version Summer 2014
+ *
+ */
+public class PileGetSticksLeft {
+
+	/**
+	 * Tests that 1 stick is left in the pile.
+	 */
+	@Test
+	public void testWith1StickPileShouldHave1StickLeft() {
+		Pile oneStickPile = new Pile(1);
+		assertEquals(1, oneStickPile.getSticksLeft());
+	}
+	
+	/**
+	 * Tests that 5 sticks are left in the pile.
+	 */
+	@Test
+	public void testWith5SticksPileShouldHave5SticksLeft() {
+		Pile fiveSticksPile = new Pile(5);
+		assertEquals(5, fiveSticksPile.getSticksLeft());
+	}
+
+}
