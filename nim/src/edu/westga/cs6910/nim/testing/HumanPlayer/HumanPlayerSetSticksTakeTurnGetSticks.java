88572877a1ea0862abde1041b1e49ffc0631e4c8
april_000 <april_000@localhost>
2014-06-16 14:31:57 -0400

    Initial Commit

---
+++ nim/src/edu/westga/cs6910/nim/testing/HumanPlayer/HumanPlayerSetSticksTakeTurnGetSticks.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.westga.cs6910.nim.testing.HumanPlayer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author april_000
+ *
+ */
+public class HumanPlayerSetSticksTakeTurnGetSticks {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
