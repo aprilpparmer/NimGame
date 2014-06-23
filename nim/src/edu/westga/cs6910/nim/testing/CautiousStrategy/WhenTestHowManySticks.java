7901156660720ed1fa933aa4a5763a16fcf65675
april_000 <april_000@localhost>
2014-06-23 19:17:59 -0400

    Initial commit.
    Passed first test - returns 1 stick on a pile size of 10

---
+++ nim/src/edu/westga/cs6910/nim/testing/CautiousStrategy/WhenTestHowManySticks.java
@@ -0,0 +1,27 @@
+package edu.westga.cs6910.nim.testing.CautiousStrategy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.westga.cs6910.nim.model.CautiousStrategy;
+
+/**
+ * Tests the functionality of the howManySticks method of the CautiousStrategy class.
+ * 
+ * @author April Parmer
+ * @version Summer 2014
+ *
+ */
+public class WhenTestHowManySticks {
+
+	/**
+	 * Tests that 1 stick is returned on a pile size of 10.
+	 */
+	@Test
+	public void testReturn1StickPile10() {
+		CautiousStrategy game = new CautiousStrategy();
+		assertEquals(1, game.howManySticks(10));
+	}
+
+}
