17db45eb92001eb4217b044addef29c860687df5
april_000 <april_000@localhost>
2014-06-24 09:30:36 -0400

    Added howManySticks method to return a random number between 1 and the pileSize - 1

--- nim/src/edu/westga/cs6910/nim/model/RandomStrategy.java
+++ nim/src/edu/westga/cs6910/nim/model/RandomStrategy.java
@@ -3,6 +3,8 @@
  */
 package edu.westga.cs6910.nim.model;
 
+import java.util.Random;
+
 /**
  * Implements the game-play strategy that returns a random number 
  * 		between 1 and the maximum number allowed, inclusive, as the 
@@ -12,6 +14,30 @@ package edu.westga.cs6910.nim.model;
  * @version Summer 2014
  *
  */
-public class RandomStrategy {
-
+public class RandomStrategy implements NumberOfSticksStrategy {
+	private Random rand;
+	
+	/**
+	 * Constructor that instantiates the Random instance variable.
+	 */
+	public RandomStrategy() {
+		this.rand = new Random();
+	}
+	
+	/**
+	 * Implements NumberOfSticksStrategy's howManySticks() to return a random number of sticks
+	 * 		to be taken from the pile.
+	 * 
+	 * Precondition: pileSize > 0
+	 * 
+	 * @param pileSize - the current size of the pile
+	 * @return randomNumber between 1 and Math.min(pileSize-1, Game.MAX_STICKS_PER_TURN), inclusive
+	 */
+	public int howManySticks(int pileSize) {
+		if (pileSize <= 0) {
+			throw new IllegalArgumentException("The pile size must be greater than 0");
+		}
+		int randomNumber = this.rand.nextInt(pileSize - 2) + 1;
+		return randomNumber;
+	}
 }
