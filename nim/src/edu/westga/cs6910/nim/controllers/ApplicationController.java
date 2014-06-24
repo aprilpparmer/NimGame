5be96449dafb91763b828c0a0837de4479bbf595
april_000 <april_000@localhost>
2014-06-24 09:04:50 -0400

    Added CautiousStrategy object and passed it to the ComputerPlayer object

--- nim/src/edu/westga/cs6910/nim/controllers/ApplicationController.java
+++ nim/src/edu/westga/cs6910/nim/controllers/ApplicationController.java
@@ -3,6 +3,7 @@
  */
 package edu.westga.cs6910.nim.controllers;
 
+import edu.westga.cs6910.nim.model.CautiousStrategy;
 import edu.westga.cs6910.nim.model.ComputerPlayer;
 import edu.westga.cs6910.nim.model.Game;
 import edu.westga.cs6910.nim.model.HumanPlayer;
@@ -29,8 +30,9 @@ public class ApplicationController {
 	public static void main(String[] args) {		
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
+				CautiousStrategy strategy = new CautiousStrategy();
 				new Gui(new Game(new HumanPlayer("Me"), 
-								 new ComputerPlayer()));
+								 new ComputerPlayer(strategy)));
 			}
 		});
 	}
