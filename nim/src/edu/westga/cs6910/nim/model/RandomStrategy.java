6d4167740b6f4b715242bb6dfe8a4428e78f22c7
april_000 <april_000@localhost>
2014-06-24 09:19:37 -0400

    Initial commit

---
+++ nim/src/edu/westga/cs6910/nim/model/RandomStrategy.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package edu.westga.cs6910.nim.model;
+
+/**
+ * Implements the game-play strategy that returns a random number 
+ * 		between 1 and the maximum number allowed, inclusive, as the 
+ * 		number of sticks to be taken from the pile.
+ * 
+ * @author April Parmer
+ * @version Summer 2014
+ *
+ */
+public class RandomStrategy {
+
+}
